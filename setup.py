8e47bfb96ade5c6d020d0326e6151b740bde89a7
sonal chaudhary <sonalkuka14@gmail.com>
2025-09-18 16:39:35 +0530

    This is my second commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirement(file:str)->list[str]:
+    requirement=[]
+    with open(file) as file_obj:
+        requirement=file_obj.readlines()
+        requirement=[req.replace("\n"," ") for req in requirement]
+
+    if '-e .' in requirement:
+        requirement.remove('-e .')
+    return requirement
+
+
+setup (
+    name= "mlproject"
+    version= '0.0.1'
+    author= "Sonal"
+    author_email= "sonalkuka14@gmail.com"
+    packages=find_packages()
+    install_requires=get_requirement('requirements.txt')
+)
